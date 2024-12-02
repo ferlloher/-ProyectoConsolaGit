bac36050531c7d3d8386fa40e9a10e231030be1e
ferlloher <ferlloher@edu.gva.es>
2024-12-02 18:15:13 +0100

    Creados archivo de bienvenida y despedida

---
+++ src/MensajeOperaciones.java
@@ -0,0 +1,7 @@
+public class MensajeOperaciones {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido");
+        System.out.println("Hasta pronto");
+    }
+    
+}
